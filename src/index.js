35b8f9541013042ec86408b3b2444f26f25641fe
Rahul Punase <rahul.punase@rahul.punase-mbp>
2022-09-25 11:24:22 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import { getTime } from "./normal/normal";
+import "./styles/styles.scss";
+
+console.log("Date: ", getTime());
+console.log("print this also");
+console.log([1, 2, 3, [4, 6]].flat());
---
+++ webpack.config.js
@@ -0,0 +1,71 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const WebpackBundleAnalyzer =
+  require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
+
+module.exports = {
+  mode: "development",
+  entry: path.resolve(__dirname, "src/index.js"),
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.[contenthash].js",
+    clean: true,
+  },
+  devtool: "source-map",
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, "dist"),
+    },
+    port: 3000,
+    open: false,
+    hot: true,
+    compress: true,
+    historyApiFallback: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            babelrc: false,
+            /**
+             * https://babeljs.io/docs/en/usage
+             */
+            presets: [
+              [
+                "@babel/preset-env",
+                {
+                  corejs: 3,
+                  useBuiltIns: "entry",
+                  targets: {
+                    edge: "17",
+                    firefox: "60",
+                    chrome: "67",
+                    safari: "11.1",
+                    ie: "11",
+                  },
+                },
+              ],
+            ],
+          },
+        },
+      },
+    ],
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Webpack App",
+      fileName: "index.html",
+      template: "src/template.html",
+    }),
+    new WebpackBundleAnalyzer(),
+  ],
+};
